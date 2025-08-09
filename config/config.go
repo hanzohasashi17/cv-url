ddd49eacab53270eb1100e9a98b013f9679d419a
Aidos <nomadbaj@gmail.com>
2025-08-09 18:13:12 +0500

    test

--- config/config.go
+++ config/config.go
@@ -1,6 +1,5 @@
 package config
 
 const ServerPort string = ":8080"
-// const MailTo string = "szhaisan@wtotem.com"
+const MailTo string = "szhaisan@wtotem.com"
 // const MailTo string = "sabitov.olzhas@wtotem.com"
-const MailTo string = "nomadbaj@gmail.com"
